14c38ab7cf3e5c44534351e7e94b7f479da1184e
Maciej <m.zientarski@kezzler.com>
2016-09-28 15:20:04 +0200

    min-date validation fix: introduced validator that fails if selected date is after provided date (in ISO format).

--- src/datetimepicker.js
+++ src/datetimepicker.js
@@ -1,55 +1,84 @@
 'use strict';
 
 angular.module('dateTimePicker', ['mgcrea.ngStrap.timepicker', 'mgcrea.ngStrap.datepicker']).directive('dateTimePicker', function() {
-	return {
-		restrict: 'E',
-		templateUrl: 'templates/datetimepicker.html',
-		scope: {
-			ngModel: '=',
-			dateFormat: '@',
-			showTimePicker: '@',
-			timeFormat: '@',
-			inputClass: '@',
-			ngDisabled: '=',
-			ngRequired: '=',
-			minDate: '@',
-			dateType: '@',
-            startWeek: '@'
-		},
-		require: ['ngModel'],
-		link: {
-			pre: function(scope){
-				scope.dateFormat = scope.dateFormat || 'yyyy-MM-dd';
-				scope.timeFormat = scope.timeFormat || 'HH:mm:ss';
-				if(typeof(scope.showTimePicker) === 'undefined'){
-					scope.showTimePicker = true;
-				} else {
-					scope.showTimePicker = scope.showTimePicker || (scope.showTimePicker === 'true');
-				}
-			}
-		}
-	};
+  return {
+    restrict: 'E',
+    templateUrl: 'templates/datetimepicker.html',
+    scope: {
+      ngModel: '=',
+      dateFormat: '@',
+      showTimePicker: '@',
+      timeFormat: '@',
+      inputClass: '@',
+      ngDisabled: '=',
+      ngRequired: '=',
+      minDate: '@',
+      dateType: '@',
+      startWeek: '@'
+    },
+    require: ['ngModel'],
+    link: {
+      pre: function(scope) {
+        scope.dateFormat = scope.dateFormat || 'yyyy-MM-dd';
+        scope.timeFormat = scope.timeFormat || 'HH:mm:ss';
+        if (typeof(scope.showTimePicker) === 'undefined') {
+          scope.showTimePicker = true;
+        } else {
+          scope.showTimePicker = scope.showTimePicker || (scope.showTimePicker === 'true');
+        }
+        scope.startOfDay = function(date) {
+          var dateStr = date.replace(/"/g, '');
+          return new Date(dateStr).setHours(0, 0, 0, 0);
+        };
+      },
+      post: function(scope, e, a, ctrls) {
+        scope.$watch('minDate', function(v) {
+          ctrls.filter(function(ctrl) {
+            return ctrl.$validators.minDate;
+          }).forEach(function(ctrl) {
+            ctrl.$validate();
+          });
+        });
+
+        ctrls.forEach(function(ctrl) {
+          ctrl.$options = ctrl.$options || {}
+          ctrl.$options.allowInvalid = true;
+          ctrl.$validators.minDate = function(value) {
+            if (!scope.minDate || scope.minDate === 'today') {
+              return true;
+            }
+            if (!value) {
+              return true;
+            }
+            var minDate = new Date(scope.minDate.replace(/"/g, ''));
+            var valid = value.getTime() >= minDate.getTime();
+            return valid;
+          }
+        });
+      }
+    }
+  };
 });
 
 angular.module('schemaForm').config(['schemaFormProvider', 'schemaFormDecoratorsProvider', 'sfPathProvider',
-	function(schemaFormProvider,  schemaFormDecoratorsProvider, sfPathProvider) {
-		var datepicker = function(name, schema, options) {
-			if (schema.type === 'string' && (schema.format === 'date-time' || schema.format === 'date')) {
-				var f = schemaFormProvider.stdFormObj(name, schema, options);
-				f.key  = options.path;
-				f.type = 'datepicker';
-				options.lookup[sfPathProvider.stringify(options.path)] = f;
-				return f;
-			}
-		};
+  function(schemaFormProvider, schemaFormDecoratorsProvider, sfPathProvider) {
+    var datepicker = function(name, schema, options) {
+      if (schema.type === 'string' && (schema.format === 'date-time' || schema.format === 'date')) {
+        var f = schemaFormProvider.stdFormObj(name, schema, options);
+        f.key = options.path;
+        f.type = 'datepicker';
+        options.lookup[sfPathProvider.stringify(options.path)] = f;
+        return f;
+      }
+    };
 
-		schemaFormProvider.defaults.string.unshift(datepicker);
+    schemaFormProvider.defaults.string.unshift(datepicker);
 
-		schemaFormDecoratorsProvider.addMapping(
-			'bootstrapDecorator',
-			'datepicker',
-			'templates/form-datepicker.html'
-		);
+    schemaFormDecoratorsProvider.addMapping(
+      'bootstrapDecorator',
+      'datepicker',
+      'templates/form-datepicker.html'
+    );
 
-	}
+  }
 ]);
